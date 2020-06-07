46c7a4078da8dfce05e0cfedc36364ed97950033
Gaurav Shakya <Hartik363@gmail.com>
2020-06-07 14:26:49 +0530

    git.java added

---
+++ Git.java
@@ -0,0 +1,13 @@
+package gitlearn;
+
+public class Git {
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			System.out.println(i);
+			
+		}
+		
+		System.out.println("i made somechanges in git.java class");
+	}
+
+}
